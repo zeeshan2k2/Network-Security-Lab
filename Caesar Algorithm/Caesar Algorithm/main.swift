f01030f1da842e711bc466f75f26284e9d43215c
Zeeshan <zeeshan.12.zw@gmail.com>
2024-07-30 03:54:11 +0500

    lab 1 done

--- Caesar Algorithm/Caesar Algorithm/main.swift
+++ Caesar Algorithm/Caesar Algorithm/main.swift
@@ -3,21 +3,23 @@ import Darwin
 
 
 let alphabets: [String] = [  "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G",
-                    "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N",
-                    "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U",
-                    "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"    
-]
+                             "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N",
+                             "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U",
+                             "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"                         ]
 
-let specialCharacters: [String] = [
-    "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",
-    "[", "]", "{", "}", "\\", "|", ";", ":", "'", "\"", ",", ".", "<", ">",
-    "/", "?", "`", "~", "§", "±", "•", "∞", "¶", "€", "£", "¥", "©", "®", "™"
-]
-
+let specialCharacters: [String] = [   "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",
+                                      "[", "]", "{", "}", "\\", "|", ";", ":", "'", "\"", ",", ".", "<", ">",
+                                      "/", "?", "`", "~", "§", "±", "•", "∞", "¶", "€", "£", "¥", "©", "®", "™"      ]
 
 print("Would you like to encode (1) or decode (2)? (Type the corresponding number): ")
 let yourChoice = readLine()
 
+if yourChoice != "1" && yourChoice != "2" {
+    print("Invalid input!")
+    exit(0)
+}
+
+print()
 print("Enter the string: ")
 let enteredString = readLine()
 
@@ -30,98 +32,35 @@ let words = enteredString!.split(separator: "")
 
 var wordArray: [String] = []
 var sentence: String = ""
+let encodedOrDecoded = ["Encoded", "Decoded"]
 
-
+var num: Int = 0
 
 func implementation(_: String) {
     for word in words {
         let instances = word.split(separator: "")
         var instanceNumber = 0
-        if yourChoice == "1" {
-            for instance in instances {
-                if alphabets.contains(String(instance)) {
-                    instanceNumber = alphabets.firstIndex(of: String(instance))!
-                    if instanceNumber < 46 {
-                        wordArray.append(alphabets[instanceNumber + 6])
-                    } else if instanceNumber >= 46 {
-                        instanceNumber = instanceNumber - 46
-                        wordArray.append(alphabets[instanceNumber])
-                    }
-                } else if specialCharacters.contains(String(instance)) {
-                    wordArray.append(String(instance))
-                } else {
-                    wordArray.append(String(instance))
+        for instance in instances {
+            if alphabets.contains(String(instance)) {
+                num = (yourChoice == "1") ? 46 : 6
+                instanceNumber = alphabets.firstIndex(of: String(instance))!
+                if instanceNumber < num {
+                    wordArray.append(alphabets[num == 46 ? instanceNumber + 6 : instanceNumber + 46])
+                } else if instanceNumber >= num {
+                    wordArray.append(alphabets[instanceNumber - num])
                 }
+            } else if specialCharacters.contains(String(instance)) {
+                wordArray.append(String(instance))
+            } else {
+                wordArray.append(String(instance))
             }
-        } else if yourChoice == "2" {
-            for instance in instances {
-                if alphabets.contains(String(instance)) {
-                    instanceNumber = alphabets.firstIndex(of: String(instance))!
-                    if instanceNumber >= 6 {
-                        wordArray.append(alphabets[instanceNumber - 6])
-                    } else if instanceNumber < 6 {
-                        wordArray.append(alphabets[51 - instanceNumber])
-                    }
-                } else if specialCharacters.contains(String(instance)) {
-                    wordArray.append(String(instance))
-                } else {
-                    wordArray.append(String(instance))
-                }
-            }
-        } else {
-            print("You have entered an invalid number!")
-            exit(0)
         }
     }
     sentence = wordArray.joined()
-    print("This is the answer string: \(sentence)")
+    print()
+    print("This is the entered string: \(enteredString!)")
+    print("This is the \(encodedOrDecoded[(Int(yourChoice!)!) - 1]) string: \(sentence)")
+    print()
 }
 
 implementation(yourChoice!)
-
-//for word in words {
-//    let instances = word.split(separator: "")
-//    var instanceNumber = 0
-//    if yourChoice == "1" {
-//        for instance in instances {
-//            if alphabets.contains(String(instance)) {
-//                instanceNumber = alphabets.firstIndex(of: String(instance))!
-//                if instanceNumber < 46 {
-//                    wordArray.append(alphabets[instanceNumber + 6])
-//                } else if instanceNumber >= 46 {
-//                    instanceNumber = instanceNumber - 46
-//                    wordArray.append(alphabets[instanceNumber])
-//                }
-//            } else if specialCharacters.contains(String(instance)) {
-//                wordArray.append(String(instance))
-//            } else {
-//                wordArray.append(String(instance))
-//            }
-//        }
-//    } else if yourChoice == "2" {
-//        for instance in instances {
-//            if alphabets.contains(String(instance)) {
-//                instanceNumber = alphabets.firstIndex(of: String(instance))!
-//                if instanceNumber >= 6 {
-//                    wordArray.append(alphabets[instanceNumber - 6])
-//                } else if instanceNumber < 6 {
-//                    wordArray.append(alphabets[51 - instanceNumber])
-//                }
-//            } else if specialCharacters.contains(String(instance)) {
-//                wordArray.append(String(instance))
-//            } else {
-//                wordArray.append(String(instance))
-//            }
-//        }
-//    } else {
-//        print("You have entered an invalid number!")
-//        exit(0)
-//    }
-//}
-
-//sentence = wordArray.joined()
-
-//print(wordArray)
-
-//print("This is the answer string: \(sentence)")
-
