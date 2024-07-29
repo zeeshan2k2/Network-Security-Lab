e93b5f7d697c284d72342da2090f9137a7aa810b
Zeeshan <zeeshan.12.zw@gmail.com>
2024-07-30 02:22:40 +0500

    added encoding method

--- Caesar Algorithm/Caesar Algorithm/main.swift
+++ Caesar Algorithm/Caesar Algorithm/main.swift
@@ -9,38 +9,64 @@ import Foundation
 import Darwin
 
 
-
 let alphabets: [String] = [  "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G",
                     "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N",
                     "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U",
-                    "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"                      ]
+                    "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"    
+]
 
 
-
-// Taking input from the user
-//print("Enter some text:")
-//if let userInput = readLine(), !userInput.isEmpty {
-//    // Printing the input
-//    print("You entered: \(userInput)")
-//} else {
-//    // No input received, exiting the program
-//    print("No input received.")
-//    exit(0) // Exiting the program with status code 0
-//}
+let specialCharacters: [String] = [
+    "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",
+    "[", "]", "{", "}", "\\", "|", ";", ":", "'", "\"", ",", ".", "<", ">",
+    "/", "?", "`", "~", "§", "±", "•", "∞", "¶", "€", "£", "¥", "©", "®", "™"
+]
 
 
 let enteredString = "Test String for Encoding XXZZAABB ..?"
 
-let words = enteredString.split(separator: " ")
+let words = enteredString.split(separator: "")
+//let words = enteredString.map { String($0) }
+var wordArray: [String] = []
+var sentence: String = ""
 
 //print(word)
 
+var count = 0 // to keep count
+
 for word in words {
     var instances = word.split(separator: "")
+    var instanceNumber = 0
     for instance in instances {
-        print(instance)
+        count = 0
+        if alphabets.contains(String(instance)) {
+//            print(alphabets.firstIndex(of: String(instance))!)
+            instanceNumber = alphabets.firstIndex(of: String(instance))!
+            if instanceNumber < 46 {
+                wordArray.append(alphabets[instanceNumber + 6])
+            } else if instanceNumber >= 46 {
+                instanceNumber = instanceNumber - 46
+                wordArray.append(alphabets[instanceNumber])
+                print(instance)
+            }
+//            wordArray.append(alphabets.firstIndex(of: String(instance)))
+//            print("\(instance)")
+        } else if specialCharacters.contains(String(instance)) {
+            wordArray.append(String(instance))
+//            print(instance)
+        } else {
+            wordArray.append(String(instance))
+//            print(instance)
+        }
+        count += 1
     }
 }
 
+sentence = wordArray.joined()
+
+print(wordArray)
+
+print("This is the sentence: \(sentence)")
 
 
+print("Whvw Vwulqj iru Hqfrglqj AACCDDEE ..?")
